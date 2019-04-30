22c90a284c8f032efe0a400476b4fbaa31750ae2
sbx_user1051 <sbx_user1051@169.254.40.129>
2019-04-30 16:23:10 +0000

    Tue Apr 30 16:23:10 UTC 2019

--- src/test/scala/befaster/solutions/SUM/SumTest.scala
+++ src/test/scala/befaster/solutions/SUM/SumTest.scala
@@ -7,4 +7,16 @@ class SumTest extends FlatSpec with Matchers {
   it should "compute sum" in {
     Sum.sum(1,2) shouldBe 3
   }
+
+  it should "throw exception if par negative" in {
+    intercept[IllegalArgumentException]{
+      Sum.sum(-2,10)
+    }}
+
+    it should "throw exception if par bigger than 100" in {
+      intercept[IllegalArgumentException]{
+        Sum.sum(1,105)
+      }}
+
 }
+
