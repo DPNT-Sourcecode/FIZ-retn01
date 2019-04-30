0f0632302e163686babdf27e549b15f7ae90740f
sbx_user1051 <sbx_user1051@169.254.40.129>
2019-04-30 16:36:56 +0000

    Tue Apr 30 16:36:56 UTC 2019

---
+++ src/test/scala/befaster/solutions/FIZ/FizTest.scala
@@ -0,0 +1,21 @@
+package befaster.solutions.SUM
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SumTest extends FlatSpec with Matchers {
+
+  it should "compute sum" in {
+    Sum.sum(1,2) shouldBe 3
+  }
+
+  it should "throw exception if par negative" in {
+    intercept[IllegalArgumentException]{
+      Sum.sum(-2,10)
+    }}
+
+    it should "throw exception if par bigger than 100" in {
+      intercept[IllegalArgumentException]{
+        Sum.sum(1,105)
+      }}
+
+}
