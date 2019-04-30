d51fd1617cd76de96669b267f0963952472e2dfd
sbx_user1051 <sbx_user1051@169.254.40.129>
2019-04-30 16:33:56 +0000

    Tue Apr 30 16:33:56 UTC 2019

--- src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
+++ src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
@@ -3,5 +3,14 @@ package befaster.solutions.FIZ
 import befaster.runner.SolutionNotImplementedException
 
 object FizzBuzz {
-  def fizzBuzz(number: Int): String = throw new SolutionNotImplementedException()
+  def fizzBuzz(number: Int): String ={
+    if(number<1 || number>9999) throw new IllegalArgumentException("Number should be in [1-9999]")
+    (number%3,number%5) match {
+      case (0,0) => "FizzBuzz"
+      case (0,_) => "Fizz"
+      case (_,0) => "Buzz"
+      case _ => ""+number
+    }
+  }
 }
+
