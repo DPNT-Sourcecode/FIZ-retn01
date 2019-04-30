3034b68325e606c9ea15f9c9b8e4f21a89db7e44
sbx_user1051 <sbx_user1051@169.254.204.125>
2019-04-30 09:36:01 +0000

    Tue Apr 30 09:36:01 UTC 2019

--- src/test/scala/befaster/solutions/SUM/SumTest.scala
+++ src/test/scala/befaster/solutions/SUM/SumTest.scala
@@ -7,4 +7,11 @@ class SumTest extends FlatSpec with Matchers {
   it should "compute sum" in {
     Sum.sum(1,2) shouldBe 3
   }
+
+
+  it should "compute sum with negative" in {
+    Sum.sum(-10,-20) shouldBe -30
+  }
+
 }
+
