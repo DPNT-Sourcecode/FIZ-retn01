d9b480ce8f77e67067d7d1310c10790734d874e6
sbx_user1051 <sbx_user1051@169.254.40.129>
2019-04-30 17:04:05 +0000

    Tue Apr 30 17:04:05 UTC 2019

--- src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
+++ src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
@@ -8,9 +8,10 @@ object FizzBuzz {
     val numberStr=number.toString
     val isFizz=number%3==0 || numberStr.contains('3')
     val isBuzz=number%5==0 || numberStr.contains('5')
-    val isDeluxe=number>10 && numberStr.count(x=> x==numberStr.charAt(0))==numberStr.length()  && number%2==0
+    val isDeluxe=number>10 && numberStr.count(x=> x==numberStr.charAt(0))==numberStr.length()
+    val isFakeDeluxe=isDeluxe &&  number%2==1
     (isFizz,isBuzz,isDeluxe) match {
-      case (true,true,true) => "fizz buzz deluxe"
+      case (true,true,true) => if (isFakeDeluxe)"fizz buzz fake deluxe" else "fizz buzz deluxe"
       case (true,true,false) => "fizz buzz"
       case (true,false,true) => "fizz deluxe"
       case (false,true,true) => "buzz deluxe"
@@ -21,4 +22,5 @@ object FizzBuzz {
     }
   }
 }
+
 
