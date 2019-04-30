d5d60c6750f5505b7388ba36c565d1ff14d9c14a
sbx_user1051 <sbx_user1051@169.254.122.197>
2019-04-30 16:08:51 +0000

    Tue Apr 30 16:08:51 UTC 2019

--- src/main/scala/befaster/solutions/SUM/Sum.scala
+++ src/main/scala/befaster/solutions/SUM/Sum.scala
@@ -3,11 +3,5 @@ package befaster.solutions.SUM
 import befaster.runner.SolutionNotImplementedException
 
 object Sum {
-  def sum(x: Int, y: Int): Int = {
-     if (x<=0 || x>=100) throw new IllegalArgumentException("first parameter Must be  >=0 adn <=100")
-    if (y<=0 || y>=100) throw new IllegalArgumentException("second parameter Must be  >=0 adn <=100")
-    x+y
-  }
+  def sum(x: Int, y: Int): Int = throw new SolutionNotImplementedException()
 }
-
-
--- src/test/scala/befaster/solutions/SUM/SumTest.scala
+++ src/test/scala/befaster/solutions/SUM/SumTest.scala
@@ -7,19 +7,4 @@ class SumTest extends FlatSpec with Matchers {
   it should "compute sum" in {
     Sum.sum(1,2) shouldBe 3
   }
-
-
-  it should "thow exception if x negative " in {
-    intercept[IllegalArgumentException] {
-      Sum.sum(-1,10)
-    }
-  }
-
-  it should "thow exception if y bigger than 100 " in {
-    intercept[IllegalArgumentException] {
-      Sum.sum(-1,1000)
-    }
-  }
 }
-
-
