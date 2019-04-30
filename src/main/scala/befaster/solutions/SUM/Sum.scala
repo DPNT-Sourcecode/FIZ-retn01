1ed8045ee85a054558e7abefab5ecc86d2498bf5
sbx_user1051 <sbx_user1051@169.254.40.129>
2019-04-30 16:24:34 +0000

    Tue Apr 30 16:24:34 UTC 2019

--- src/main/scala/befaster/solutions/SUM/Sum.scala
+++ src/main/scala/befaster/solutions/SUM/Sum.scala
@@ -4,10 +4,11 @@ import befaster.runner.SolutionNotImplementedException
 
 object Sum {
   def sum(x: Int, y: Int): Int ={
-    if(x<0 && x>100) throw new IllegalArgumentException("First argument is out of range")
-    if(y<0 && x>100) throw new IllegalArgumentException("Second argument is out of range")
+    if(x<0 || x>100) throw new IllegalArgumentException("First argument is out of range")
+    if(y<0 || y>100) throw new IllegalArgumentException("Second argument is out of range")
     x+y
   }
 }
+
 
 
